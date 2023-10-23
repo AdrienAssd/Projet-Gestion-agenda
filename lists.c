e217afd4c5a83488e8036785927808b483c34cf8
Thibault BIAL <70467226+ThiBIAL@users.noreply.github.com>
2023-10-23 18:32:32 +0200

    Update lists.c

--- lists.c
+++ lists.c
@@ -1,5 +1,13 @@
-//
-// Created by Adrien Assouad on 23/10/2023.
-//
-
 #include "lists.h"
+
+t_d_list *createEmptyList(int niv_max)
+{
+    t_d_list *newlist = (t_d_list*)malloc(sizeof(t_d_list));
+    newlist->niv_max = niv_max;
+    newlist->head = (t_d_cell*)malloc(newlist->niv_max*sizeof(t_d_cell));
+    for(int i = 0 ; i < newlist->niv_max ; i++)
+    {
+        newlist->head[i] = NULL;
+    }
+    return newlist;
+}
