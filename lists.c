d1ff713784b42c6f0fb9b340661076130fafd1bd
Adrien <adrien.assouad@icloud.com>
2023-10-31 23:55:50 +0100

    Partie 1 finie

--- lists.c
+++ lists.c
@@ -20,15 +20,27 @@ void addHeadList(t_d_list * list, t_d_cell * cell){
             list->head[i] = cell;
         }
         else{
-            t_d_cell * temp;
-            temp = cell;
-            temp->next[i] = list->head[i];
-            list->head[i] = temp;
+            t_d_cell * temp, *prev;
+            temp = list->head[i];
+            prev = NULL;
+            while (temp != NULL && temp->value <cell->value){
+                prev = temp;
+                temp=temp->next[i];
+            }
+            if (prev == NULL){
+                cell->next[i] = list->head[i];
+                list->head[i] = cell;
+            }
+            else{
+                prev->next[i] = cell;
+                cell->next[i] = temp;
+            }
         }
     }
 }
 
-void displayList(t_d_list list){
+
+/*void displayList(t_d_list list){
     t_d_cell * temp;
     for (int i = 0; i <list.niv_max; i++){
         temp = list.head[i];
@@ -40,16 +52,47 @@ void displayList(t_d_list list){
         printf("NULL\n");
     }
     printf("\n");
+}*/
+
+void displayList(t_d_list list){
+    t_d_cell * temp, *temp2;
+    for (int i = 0; i <list.niv_max; i++){
+        temp = list.head[i];
+        temp2 = list.head[0];
+        printf("[list head_%d @-]", i);
+        while (temp != NULL){
+            while (temp->value != temp2->value){
+                printf("-----------");
+                temp2 = temp2->next[0];
+            }
+            printf("-->[ %d|@-]", temp->value);
+            temp = temp->next[i];
+            temp2 = temp2->next[0];
+        }
+        while(temp2!=NULL && temp==NULL){
+            printf("-----------");
+            temp2 = temp2->next[0];
+        }
+        printf("-->NULL\n");
+    }
+    printf("\n");
 }
+
+
+
+
+
+
+
 
 
 t_d_list cellinlist(){
     int nbValeur, nbNiveau;
     t_d_list * list;
     t_d_cell * newcell;
-    printf("combien de valeurs voulez-vous ?");
+    printf("combien de valeurs voulez-vous ? ");
     scanf("%d", &nbValeur);
-    printf("combien de niveaux voulez-vous ?");
+    printf("combien de niveaux voulez-vous ? ");
     scanf("%d", &nbNiveau);
     list = createEmptyList(nbNiveau);
     for (int i = 0; i < nbValeur; i++){
