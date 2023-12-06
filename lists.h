14388da7c042b6ec55c9f7781240118b05b878c3
Thibault BIAL <70467226+ThiBIAL@users.noreply.github.com>
2023-12-06 15:49:06 +0100

    Ajout Commentaires lists.h

--- lists.h
+++ lists.h
@@ -4,15 +4,18 @@
 #include <stdlib.h>
 #include "cells.h"
 
+// Structure de la liste
 typedef struct s_d_list
 {
     int niv_max;
     t_d_cell **head;
 } t_d_list;
 
+// Prototypes des fonctions
 t_d_list *createEmptyList(int niv_max);
 void addHeadSortList(t_d_list * list, t_d_cell * cell);
 void displayList(t_d_list list);
 t_d_list cellinlist();
 t_d_list createListNiv(int n);
+
 #endif //PROJET_SDD_LISTS_H
