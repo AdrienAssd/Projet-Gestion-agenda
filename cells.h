8bf247e81a585d6a8f33701454c1e7f5147b0003
Thibault BIAL <70467226+ThiBIAL@users.noreply.github.com>
2023-12-06 15:43:40 +0100

    Ajout commentaires cells.h

--- cells.h
+++ cells.h
@@ -3,16 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+// Structure de la cellule
 typedef struct s_d_cell
 {
     int value;
     int niveau;
     struct s_d_cell **next;
-
 } t_d_cell;
 
-
+// Prototype des fonctions
 t_d_cell * createCell(int valeur , int niveau);
 t_d_cell * user_choice_cell();
 
