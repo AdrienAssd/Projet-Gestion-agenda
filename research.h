6330f277f2a629ee90bb1c26cf750c1d77a43616
Thibault BIAL <70467226+ThiBIAL@users.noreply.github.com>
2023-12-06 15:51:52 +0100

    Ajout Commentaires research.h

--- research.h
+++ research.h
@@ -1,12 +1,10 @@
-//
-// Created by Adrien Assouad on 07/11/2023.
-//
-
 #ifndef GESTION_AGENDA_RESEARCH_H
 #define GESTION_AGENDA_RESEARCH_H
 #include "lists.h"
 
+// Prototypes des fonctions
 int researchclassique (t_d_list list, int val);
 int researchNivHaut(t_d_list list, int val);
 void timeResearch();
+
 #endif //GESTION_AGENDA_RESEARCH_H
